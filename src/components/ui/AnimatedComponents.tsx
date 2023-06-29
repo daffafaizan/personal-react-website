058802fed3ece2978b991cce3ef984590cb29a1c
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-06-29 21:43:15 +0700

    Temporarily removed fade in page transition

--- src/components/ui/AnimatedComponents.tsx
+++ src/components/ui/AnimatedComponents.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function AnimatedComponents({ children }) {
   return (
     <motion.div
-      initial={{ y: 25, opacity: 0 }}
+      initial={{ y: 0, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 0.2, duration: 1 }}
     >
