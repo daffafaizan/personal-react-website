d39fc33f0e029c60a9d9ffa7a95b7d86f89ea325
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-06-30 10:28:52 +0700

    Added a sketch for the about me section

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -12,6 +12,15 @@ function Blog() {
             icon="fluent-emoji:construction"
             className="h-36 w-36 xxs:h-36 xxs:w-36 xxxs:h-36 xxxs:w-36 hover:scale-110 duration-300"
           />
+          <h1 className="title text-3xl sm:text-2xl xxs:text-2xl xxxs:text-xl dark:text-white mb-10 md:mb-10 font-bold text-justify w-9/12">
+            I was born in Bandung, raised in Cirebon, and currently pursuing my
+            studies in the University of Indonesia as a computer science major.
+            <p className="my-6" />
+            As a minimalistic
+            <p className="my-6" />
+            In my free time, I like to play competitive FPS and strategy games
+            or hang out with my friends.
+          </h1>
         </div>
       </section>
     </AnimatedComponents>
