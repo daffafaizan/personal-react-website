8a695bbe5ddc55c0923e5cdd68542923ebc3e2e9
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-02 20:55:21 +0700

    Made animated cursor into a component in ui

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -2,9 +2,9 @@ import Sidebar from "../../components/ui/Sidebar";
 import Footer from "../../components/ui/Footer";
 import Socials from "../../components/home/Socials";
 import BlogContent from "../../components/blog/BlogContent";
-import AnimatedCursor from "react-animated-cursor";
 import { useEffect } from "react";
 import AnimatedPage from "../../components/ui/AnimatedPage";
+import CustomCursor from "../../components/ui/CustomCursor";
 
 function Blog() {
   const navigation = [
@@ -20,22 +20,7 @@ function Blog() {
     <AnimatedPage>
       <div className="bg-[#F4F5F0] dark:bg-[#171717] text-stone-900 dark:text-[#EDEDED] min-h-screen font-inter scroll-smooth">
         <Sidebar navigation={navigation} />
-        <AnimatedCursor
-          color="255,255,255"
-          innerSize={12}
-          outerSize={45}
-          innerScale={1}
-          outerScale={2}
-          outerAlpha={1}
-          hasBlendMode={true}
-          outerStyle={{
-            mixBlendMode: "exclusion",
-          }}
-          innerStyle={{
-            backgroundColor: "#333",
-            mixBlendMode: "exclusion",
-          }}
-        />
+        <CustomCursor />
         <div className="max-w-6xl w-11/12 mx-auto">
           <BlogContent />
           <Socials />
