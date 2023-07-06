8cf8a4c2f68ff75f32c11d3e50b45e35bea9ff6d
Daffa Muhammad Faizan <daffa.muhammad12@ui.ac.id>
2023-07-06 20:39:29 +0700

    Added feature for sidebar to open to a new tab and added github repo link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ import Technologies from "../../components/home/Technologies";
 import Socials from "../../components/home/Socials";
 import BlogPreview from "../../components/home/BlogPreview";
 import AboutMe from "../../components/home/AboutMe";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import AnimatedPage from "../../components/ui/AnimatedPage";
 
 function Home() {
@@ -17,27 +17,60 @@ function Home() {
       name: "Technologies",
       href: "#technologies",
       path: "/technologies",
+      target: "",
+      rel: "",
       current: false,
     },
     {
       name: "Experience",
       href: "#experience",
       path: "/experience",
+      target: "",
+      rel: "",
       current: false,
     },
-    { name: "Blog", href: "#blog", path: "/blog", current: false },
-    { name: "Contact", href: "#contact", path: "/contact", current: false },
-    { name: "Projects", href: "/projects", path: "/projects", current: false },
+    {
+      name: "Blog",
+      href: "#blog",
+      path: "/blog",
+      target: "",
+      rel: "",
+      current: false,
+    },
+    {
+      name: "Contact",
+      href: "#contact",
+      path: "/contact",
+      target: "",
+      rel: "",
+      current: false,
+    },
+    {
+      name: "Projects",
+      href: "/projects",
+      path: "/projects",
+      target: "",
+      rel: "",
+      current: false,
+    },
+    {
+      name: "GitHub Repo",
+      href: "https://github.com/daffafaizan/personal-react-website",
+      path: "https://github.com/daffafaizan/personal-react-website",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      current: false,
+    },
   ];
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <AnimatedPage>
       <div className="bg-[#F4F5F0] dark:bg-[#171717] text-stone-900 dark:text-[#EDEDED] min-h-screen font-inter scroll-smooth">
-        <Sidebar navigation={navigation}/>
+        <Sidebar navigation={navigation} />
         <div className="max-w-6xl w-11/12 mx-auto">
           <Intro />
           <AboutMe />
