3f41920093b0b7be6a09ba841af51e435c3c2d79
Daffa <daffamfzn@gmail.com>
2024-01-01 09:26:47 +0700

    feat: updated copyright year

--- src/components/ui/sections/footer.tsx
+++ src/components/ui/sections/footer.tsx
@@ -4,7 +4,6 @@ function Footer() {
   return (
     <AnimatedComponents>
       <div className="py-5 text-center">
-        {/* {Social Icons} */}
         <p className="text-sm mt-2 opacity-50">
           &copy; {new Date().getFullYear()} Daffa Muhammad Faizan. All rights
           reserved.
